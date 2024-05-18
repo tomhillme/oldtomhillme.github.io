49ecf8e26ab996d9d9cfab70efcc3d62cc7e1ebe
tomhillme <hilltopchill+me@outlook.com>
2024-05-18 23:57:25 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 $(document).ready(() => {
-  $(".spotify").hover(() => {
-    $("body").css("background", "#DB3340");
-    $(".spotify").css("color", "#ffffff");
+  $(".soundcloud").hover(() => {
+    $("body").css("background", "#FF7700");
+    $(".soundcloud").css("color", "#ffffff");
     $("h1").css("color", "#ffffff");
+    $("#tomhill").css("color", "#ffffff");
   }, () => {
     $("body").css("background", "#ffffff");
-    $(".spotify").css("color", "#DB3340");
+    $(".soundcloud").css("color", "#FF7700");
     $("h1").css("color", "#000000");
+    $("#tomhill").css("color", "#000000");
+  });
+
+  $(".linkedin").hover(() => {
+    $("body").css("background", "#0077B5");
+    $(".linkedin").css("color", "#ffffff");
+    $("h1").css("color", "#ffffff");
+    $("#tomhill").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".linkedin").css("color", "#0077B5");
+    $("h1").css("color", "#000000");
+    $("#tomhill").css("color", "#000000");
   });
 
   $(".instagram").hover(() => {
-    $("body").css("background", "#E8B71A");
+    $("body").css("background", "#962fbf");
     $(".instagram").css("color", "#fff");
     $("h1").css("color", "#ffffff");
+    $("#tomhill").css("color", "#ffffff");
   }, () => {
     $("body").css("background", "#ffffff");
-    $(".instagram").css("color", "#E8B71A");
+    $(".instagram").css("color", "#962fbf");
     $("h1").css("color", "#000000");
+    $("#tomhill").css("color", "#000000");
   });
 
-  $(".whatsapp").hover(() => {
-    $("body").css("background", "#1FDA9A");
-    $(".whatsapp").css("color", "#fff");
+  $(".unsplash").hover(() => {
+    $("body").css("background", "#0cda3c");
+    $(".unsplash").css("color", "#fff");
     $("h1").css("color", "#ffffff");
+    $("#tomhill").css("color", "#ffffff");
   }, () => {
     $("body").css("background", "#ffffff");
-    $(".whatsapp").css("color", "#1FDA9A");
+    $(".unsplash").css("color", "#0cda3c");
     $("h1").css("color", "#000000");
+    $("#tomhill").css("color", "#000000");
   });
-  
-    $(".email").hover(() => {
-    $("body").css("background", "#28ABE3");
-    $(".email").css("color", "#fff");
-    $("h1").css("color", "#ffffff");
-  }, () => {
-    $("body").css("background", "#ffffff");
-    $(".email").css("color", "#28ABE3");
-    $("h1").css("color", "#000000");
-  });
+
 });
